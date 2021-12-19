46c3a2709636771ae959c06327d55d0577480eb4
Raymond Chan <rcwm831@gmail.com>
2021-12-19 18:45:54 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/rayc4/Bookstore""
    This reverts commit ebf677feca5f62c5147e3d2ccc99957e509cd0ef.

---
+++ SQL/Functions.sql
@@ -0,0 +1,17 @@
+-- get genres and their revenue given start and end date
+create function get_genre_revenue(start_date date, end_date date)
+returns table(genre varchar(20), revenue numeric(10,2))
+language plpgsql
+as $$
+begin
+    select genre, sum(book_revenue)::numeric(10,2) as revenue from book_genre natural join (
+        select isbn, (sum(bo.quantity) * price) * (1 - publisher_cut) as book_revenue
+            from book join (
+                select * from book_order
+                where order_id in
+                (select id from order_ where date between start_date and end_date)
+            ) as bo using (isbn)
+            group by isbn
+    ) as _
+    group by genre;
+end; $$
