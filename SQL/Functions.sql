38ae120c2c5342ffd7adfe78873fcd9a3af0e511
TheRealJustinC <Justinchung8723@gmail.com>
2021-12-19 20:02:16 -0500

    Update Functions.sql
    get_author_revenue from start and end date

--- SQL/Functions.sql
+++ SQL/Functions.sql
@@ -15,3 +15,22 @@ begin
     ) as _
     group by genre;
 end; $$
+
+-- get author and their revenue given start and end date
+create function get_author_revenue(start_date date, end_date date)
+returns table(author varchar(20), revenue numeric(10,2))
+language plpgsql
+as $$
+begin
+    select first_name, last_name, sum(book_revenue)::numeric(10,2) as revenue from author natural join (
+        select isbn, (sum(bo.quantity) * price) * (1 - publisher_cut) as book_revenue
+            from book join (
+                select * from book_order
+                where order_id in
+                (select id from order_ where date between start_date and end_date)
+            ) as bo using (isbn)
+            group by isbn
+    ) as _
+    group by first_name, last_name;
+end;
+$$
